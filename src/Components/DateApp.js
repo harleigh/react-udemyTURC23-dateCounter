7982551d2ffbaa4d6484f8c1f8089d55020bc0ec
harleigh <harleigh.marsh@gmail.com>
2023-09-23 11:16:01 -0700

    date is calculated based on step and count, with proper conjugated message to UI

--- src/Components/DateApp.js
+++ src/Components/DateApp.js
@@ -1,16 +1,20 @@
 /* Project based on "steps" project from TURC23 */
 import { useState } from "react";
 import { DateModifier } from "./DateModifier";
+import { DisplayDate } from "./DisplayDate";
 
 export default function DateApp() {
   const [multiplier, setMultiplier] = useState(1);
   const [count, setCount] = useState(0);
+
+  const amtShiftDays = multiplier * count;
 
   return (
     <>
       <div className="dateApp">
         <DateModifier title={"Step"} val={multiplier} setVal={setMultiplier} />
         <DateModifier title={"Count"} val={count} setVal={setCount} />
+        <DisplayDate shiftAmt={amtShiftDays} />
       </div>
     </>
   );
